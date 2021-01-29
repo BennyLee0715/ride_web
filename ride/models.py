1dc9b3f4b88e7aa0d4cfce1a0f7beb7e35d1cd06
VCM admin account <vcm@vcm-18213.vm.duke.edu>
2021-01-29 03:59:23 -0500

    test stash2

--- ride/models.py
+++ ride/models.py
@@ -7,13 +7,7 @@ class User(models.Model):
     name = models.CharField(max_length=32,unique=True)
     password = models.CharField(max_length=128)
     email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=32,choices=gender,default='��')
-    c_time = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        ordering = ['c_time']
-        verbose_name = ''
-        verbose_name_plural = ''
