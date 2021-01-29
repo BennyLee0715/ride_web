f0a6ee3ee44343001690dbd41c04cbd9ea40a5d4
VCM admin account <vcm@vcm-18213.vm.duke.edu>
2021-01-29 04:29:39 -0500

    01/29 17:29

--- ride_web/urls.py
+++ ride_web/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^home/', views.home),
     path('ride/', include('ride.urls')),
     path('admin/', admin.site.urls),
 ]
