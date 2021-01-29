1d666ccbb6952c8f4a6f2ab2fb53041797728389
VCM admin account <vcm@vcm-18235.vm.duke.edu>
2021-01-29 03:16:21 -0500

    2

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#FOR TEST
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ride_web.settings')
@@ -21,3 +21,4 @@ def main():
 if __name__ == '__main__':
     main()
 #....
+#111
